edbf207472224537b3a5a75c62f24c6074f9c7d4
SimonTod <simon.todeschini@viacesi.fr>
2017-03-29 11:55:00 +0200

    ability to add a button at at the bottom of the screen, it can be used to cancel, logout, or anything the user wants

--- README.md
+++ README.md
@@ -81,7 +81,7 @@ $lockScreen.show({
 
 ### Theming / Language
 
-See available options [here](src/lock-screen/lock-screen.js#L5-L20).
+See available options [here](src/lock-screen/lock-screen.js#L5-L24).
 
 # License
 
--- src/lock-screen/lock-screen.js
+++ src/lock-screen/lock-screen.js
@@ -7,8 +7,12 @@ const lockScreenService = ($rootScope) => {
         passcode          : settings.code,
         onCorrect         : settings.onCorrect || null,
         onWrong           : settings.onWrong || null,
+        onBottomButton    : settings.onBottomButton || null, //callback function triggered when the button at the bottom is clicked
+        bottomButton      : settings.bottomButton || false, //whether you want to display and use a button at the bottom (can be used to cancel, logout, or anything else you require)
+        abovePasscodeLabel: settings.abovePasscodeLabel || null, //set some text if you want something to be written above the passcodeLabel (I use it to write the user's name)
         passcodeLabel     : settings.passcodeLabel || 'Enter Passcode',
         touchLabel        : settings.touchLabel,
+        bottomButtonLabel : settings.bottomButtonLabel || 'Cancel', //text written in the button at the button, you can set whatever you want here
         backgroundColor   : settings.backgroundColor || '#F1F1F1',
         textColor         : settings.textColor || '#464646',
         buttonColor       : settings.buttonColor || '#F8F8F8',
@@ -36,8 +40,12 @@ const lockScreenDirective = ($timeout) => {
         scope.passcode          = data.passcode;
         scope.onCorrect         = data.onCorrect;
         scope.onWrong           = data.onWrong;
+        scope.onBottomButton    = data.onBottomButton;
+        scope.bottomButton      = data.bottomButton;
+        scope.abovePasscodeLabel= data.abovePasscodeLabel;
         scope.passcodeLabel     = data.passcodeLabel;
         scope.touchLabel        = data.touchLabel;
+        scope.bottomButtonLabel = data.bottomButtonLabel;
         scope.backgroundColor   = data.backgroundColor;
         scope.textColor         = data.textColor;
         scope.buttonColor       = data.buttonColor;
@@ -70,6 +78,10 @@ const lockScreenDirective = ($timeout) => {
       };
       scope.delete = () => {
         scope.enteredPasscode = scope.enteredPasscode.slice(0,-1);
+      };
+      scope.BottomButtonAction = () => {
+        scope.onBottomButton && scope.onBottomButton();
+        scope._showLockScreen = false;
       };
       scope.digit = (digit) => {
         scope.selected = +digit;
@@ -199,6 +211,9 @@ const lockScreenDirective = ($timeout) => {
           }
       </style>
       <div class="ILS_lock" ng-class="!_showLockScreen ?  'ILS_lock-hidden' : ''">
+        <div style="text-align: center;">
+          {{abovePasscodeLabel}}
+        </div>
         <div class="ILS_label-row">
           {{passcodeLabel}}
         </div>
@@ -228,6 +243,9 @@ const lockScreenDirective = ($timeout) => {
           <div ng-click="digit(0)" class="ILS_digit">0</div>
           <div ng-show="ACDelbuttons" ng-click="delete()" class="ILS_digit ILS_del">DEL</div>
         </div>
+        <button class="button button-small button-assertive" ng-click="BottomButtonAction()" ng-if="bottomButton">
+            {{bottomButtonLabel}}
+        </button>
       </div>
     `,
   };
